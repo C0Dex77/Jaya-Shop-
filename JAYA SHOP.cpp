079f871908c6528a9d8f3c53b7fb04837772726c
Alif wira <75455120+C0Dex77@users.noreply.github.com>
2021-03-16 17:57:11 +0800

    Add files via upload

---
+++ JAYA SHOP.cpp
@@ -0,0 +1,217 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int nokia()
+{
+	int jawab,jumlah,hasil;
+	cout<<"BERIKUT ADALAH HAPE NOKIA YANG TERSEDIA"<<endl;
+	cout<<"1.Nokia 3310 IDR 200.000"<<endl;
+	cout<<"2.Nokia X2-01 IDR.2.000.000"<<endl;
+	cout<<"3.Nokia C1 IDR.1.500.000"<<endl;
+	cout<<"4.Nokia Hidayah IDR.500.000"<<endl;
+	cin>>jawab;
+	cout<<"Masukkan Jumlah hape yang ingin dibeli : ";
+	cin>>jumlah;
+	
+	if(jawab==1){
+		cout<<"Anda telah memilih Hape Nokia 3310"<<endl;
+		cout<<"Anda membeli Hp ini sebanyak "<<jumlah<<endl;
+		hasil=200000*jumlah;
+		cout<<"Totalnya adalah "<<hasil<<endl;
+		cout<<"Terima Kasih sudah berbelanja Di JAYA SHOP"<<endl;
+		system("pause");
+	}
+	else if(jawab==2){
+		cout<<"Anda telah memilih Hape Nokia X2-01"<<endl;
+		cout<<"Anda membeli Hp ini sebanyak "<<jumlah<<endl;
+		hasil=2000000*jumlah;
+		cout<<"Totalnya adalah "<<hasil<<endl;
+		cout<<"Terima Kasih sudah berbelanja Di JAYA SHOP"<<endl;
+		system("pause");
+	}
+	
+	else if(jawab==3){
+		cout<<"Anda telah memilih Hape Nokia C1"<<endl;
+		cout<<"Anda membeli Hp ini sebanyak "<<jumlah<<endl;
+		hasil=1500000*jumlah;
+		cout<<"Totalnya adalah "<<hasil<<endl;
+		cout<<"Terima Kasih sudah berbelanja Di JAYA SHOP"<<endl;
+		system("pause");
+}
+	else if(jawab==4){
+		cout<<"Anda telah memilih Hape Nokia Hidayah"<<endl;
+		cout<<"Anda membeli Hp ini sebanyak "<<jumlah<<endl;
+		hasil=500000*jumlah;
+		cout<<"Totalnya adalah "<<hasil<<endl;
+		cout<<"Terima Kasih sudah berbelanja Di JAYA SHOP"<<endl;
+		system("pause");
+	}
+		
+		else{
+			cout<<"Maaf,Produk Tidak Tersedia"<<endl;
+			system("pause");
+		}
+	
+}
+
+int samsung()
+{
+
+int jawab,jumlah,hasil;
+	cout<<"BERIKUT ADALAH HAPE SAMSUNG YANG TERSEDIA"<<endl;
+	cout<<"1.SAMSUNG CORBY IDR.3.500.000"<<endl;
+	cout<<"2.SAMSUNG NEO IDR.4.000.000"<<endl;
+	cout<<"3.SAMSUNG GALAXY IDR.2.500.000"<<endl;
+	cout<<"4.SAMSUNG A10 IDR.4.500.000"<<endl;
+	cin>>jawab;
+	cout<<"Masukkan Jumlah hape yang ingin dibeli : ";
+	cin>>jumlah;
+	
+	if(jawab==1){
+		cout<<"Anda telah memilih Hape Samsung Corby"<<endl;
+		cout<<"Anda membeli Hp ini sebanyak "<<jumlah<<endl;
+		hasil=3500000*jumlah;
+		cout<<"Totalnya adalah "<<hasil<<endl;
+		cout<<"Terima Kasih sudah berbelanja Di JAYA SHOP"<<endl;
+		system("pause");
+	}
+	else if(jawab==2){
+		cout<<"Anda telah memilih Hape Samsung Neo"<<endl;
+		cout<<"Anda membeli Hp ini sebanyak "<<jumlah<<endl;
+		hasil=4000000*jumlah;
+		cout<<"Totalnya adalah "<<hasil<<endl;
+		cout<<"Terima Kasih sudah berbelanja Di JAYA SHOP"<<endl;
+		system("pause");
+	}
+	
+	else if(jawab==3){
+		cout<<"Anda telah memilih Hape Samsung Galaxy"<<endl;
+		cout<<"Anda membeli Hp ini sebanyak "<<jumlah<<endl;
+		hasil=2500000*jumlah;
+		cout<<"Totalnya adalah "<<hasil<<endl;
+		cout<<"Terima Kasih sudah berbelanja Di JAYA SHOP"<<endl;
+		system("pause");
+	}
+	
+	else if(jawab==4){
+		cout<<"Anda telah memilih Hape Samsung A10"<<endl;
+		cout<<"Anda membeli Hp ini sebanyak "<<jumlah<<endl;
+		hasil=4500000*jumlah;
+		cout<<"Totalnya adalah "<<hasil<<endl;
+		cout<<"Terima Kasih sudah berbelanja Di JAYA SHOP"<<endl;
+		system("pause");
+	}
+		
+		else{
+			cout<<"Maaf,Produk Tidak Tersedia"<<endl;
+			system("pause");
+		
+		}
+	}
+int Xiaomi()
+{
+int jawab,jumlah,hasil;
+	cout<<"BERIKUT ADALAH HAPE XIAOMI YANG TERSEDIA"<<endl;
+	cout<<"1.XIAOMI REDMI NOTE 5 IDR 1.900.000"<<endl;
+	cout<<"2.XIAOMI REDMI NOTE 5A IDR.2.000.000"<<endl;
+	cout<<"3.XIAOMI REDMI NOTE 8 IDR.3.500.000"<<endl;
+	cout<<"4.XIAOMI REDMI NOTE 9A IDR.3.500.000"<<endl;
+	cin>>jawab;
+	cout<<"Masukkan Jumlah hape yang ingin dibeli : ";
+	cin>>jumlah;
+	
+	if(jawab==1){
+		cout<<"Anda telah memilih Hape Xiaomi Redmi Note 5"<<endl;
+		cout<<"Anda membeli Hp ini sebanyak "<<jumlah<<endl;
+		hasil=1900000*jumlah;
+		cout<<"Totalnya adalah "<<hasil<<endl;
+		cout<<"Terima Kasih sudah berbelanja Di JAYA SHOP"<<endl;
+		system("pause");
+	}
+	else if(jawab==2){
+		cout<<"Anda telah memilih Hape Xiomi Redmi note 5a"<<endl;
+		cout<<"Anda membeli Hp ini sebanyak "<<jumlah<<endl;
+		hasil=2000000*jumlah;
+		cout<<"Totalnya adalah "<<hasil<<endl;
+		cout<<"Terima Kasih sudah berbelanja Di JAYA SHOP"<<endl;
+		system("pause");
+	}
+	
+	else if(jawab==3){
+		cout<<"Anda telah memilih Hape Xiaomi Redmi Note 8"<<endl;
+		cout<<"Anda membeli Hp ini sebanyak "<<jumlah<<endl;
+		hasil=3500000*jumlah;
+		cout<<"Totalnya adalah "<<hasil<<endl;
+		cout<<"Terima Kasih sudah berbelanja Di JAYA SHOP"<<endl;
+		system("pause");
+	}
+	
+	else if(jawab==4){
+		cout<<"Anda telah memilih Hape Redmi Note 9A"<<endl;
+		cout<<"Anda membeli Hp ini sebanyak "<<jumlah<<endl;
+		hasil=3500000*jumlah;
+		cout<<"Totalnya adalah "<<hasil<<endl;
+		cout<<"Terima Kasih sudah berbelanja Di JAYA SHOP"<<endl;
+		system("pause");
+	}
+		
+		else{
+			cout<<"Maaf,Produk Tidak Tersedia"<<endl;
+			system("pause");
+		}
+	}
+		
+int main()
+{
+		int jawab;
+		char jawab1;
+		menu:
+		cout<<"======================================"<<endl;
+		cout<<"||                                  ||"<<endl;
+		cout<<"||	SELAMAT DATANG DI JAYA SHOP ||"<<endl;
+		cout<<"||                                  ||"<<endl;
+		cout<<"======================================"<<endl;
+		
+		cout<<"SILAHKAN PILIH MERK HAPE YANG INGIN ANDA BELI"<<endl;
+		cout<<"1.Nokia"<<endl;
+		cout<<"2.Samsung"<<endl;
+		cout<<"3.Xiaomi"<<endl;
+		cin>>jawab;
+		
+		switch(jawab)
+		{
+			case 1:
+			nokia();
+				break;
+				
+				case 2:
+				samsung();
+					break;
+					
+					case 3:
+					Xiaomi();
+						break;
+						
+						default:
+							cout<<"\nSALAH!!!"<<endl;
+							
+							
+			}
+				
+				cout<<"APAKAH ANDA INGIN BERBELANJA LAGI Y/N ?"<<endl;
+				cin>>jawab1;
+				if(jawab1=='y'||'Y'){
+					goto menu;
+				}
+				
+				else if(jawab1=='N'||'n'){
+					cout<<"DATANG LAGI YA"<<endl;
+				}
+				
+				else{
+					cout<<"Salah perintah"<<endl;
+				}
+					
+				getch();
+			}
